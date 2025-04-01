710a1bd2023a75414708cd81cb50afbc1eafa3d2
zq11444 <1183681233@qq.com>
2025-04-01 16:56:11 +0800

    删除了foot组件

--- src/views/account/login.vue
+++ src/views/account/login.vue
@@ -1,7 +1,6 @@
 <template>
   <showtitle text="这里是登陆页面" />
   <navlinks />
-  <foot />
 </template>
 
 
@@ -9,14 +8,8 @@
 import { ref, reactive } from "vue"
 import showtitle from "@/components/test/showtitle.vue"
 import navlinks from "@/components/test/navlinks.vue";
-import foot from "@/components/foot.vue";
 </script>
 
 
 <style scoped>
-footer {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-}
 </style>
