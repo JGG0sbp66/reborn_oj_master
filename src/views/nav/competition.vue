7ce07635840df1e4b3dbeeeb95c07d55bdfbea38
zq11444 <1183681233@qq.com>
2025-04-01 16:57:10 +0800

    删除了foot组件

--- src/views/nav/competition.vue
+++ src/views/nav/competition.vue
@@ -1,7 +1,6 @@
 <template>
   <showtitle text="这里是竞赛页面" />
   <navlinks />
-  <foot />
 </template>
 
 
@@ -9,16 +8,9 @@
 import { ref, reactive } from "vue"
 import showtitle from "@/components/test/showtitle.vue"
 import navlinks from "@/components/test/navlinks.vue";
-import foot from "@/components/foot.vue";
-
 </script>
 
 
 <style scoped>
-footer {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-}
 </style>
 
