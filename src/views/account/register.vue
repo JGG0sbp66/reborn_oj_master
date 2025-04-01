acbc5f7b00d5c8ade5fffe671637efe8aeca179f
zq11444 <1183681233@qq.com>
2025-04-01 16:56:35 +0800

    删除了foot组件

--- src/views/account/register.vue
+++ src/views/account/register.vue
@@ -2,7 +2,6 @@
   <showtitle text="这里是注册页面" />
   <navlinks />
   <RegisterForm />
-  <foot />
 </template>
 
 
@@ -11,15 +10,8 @@ import { ref, reactive } from "vue"
 import showtitle from "@/components/test/showtitle.vue"
 import navlinks from "@/components/test/navlinks.vue";
 import RegisterForm from "@/components/RegisterForm.vue";
-import foot from "@/components/foot.vue";
-
 </script>
 
 
 <style scoped>
-footer {
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-}
 </style>
