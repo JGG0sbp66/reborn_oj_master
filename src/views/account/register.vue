67a7e2d69ab8b09a072485b444bce98721992d92
zq <1183681233@qq.com>
2025-04-01 15:59:25 +0800

    在五个页面添加了页脚组件

--- src/views/account/register.vue
+++ src/views/account/register.vue
@@ -1,22 +1,25 @@
 <template>
-    <showtitle text="这里是注册页面"/>
-    <navlinks/>
-    <RegisterForm/>
-    
-  </template>
-  
-  
-  <script setup lang="ts">
-    import {ref,reactive} from "vue"
-    import showtitle from "@/components/test/showtitle.vue"
-    import navlinks from "@/components/test/navlinks.vue";
-    import RegisterForm from "@/components/RegisterForm.vue";
+  <showtitle text="这里是注册页面" />
+  <navlinks />
+  <RegisterForm />
+  <foot />
+</template>
 
-  </script>
-  
-  
-  <style scoped>
-    
-    
-  </style>
-  
+
+<script setup lang="ts">
+import { ref, reactive } from "vue"
+import showtitle from "@/components/test/showtitle.vue"
+import navlinks from "@/components/test/navlinks.vue";
+import RegisterForm from "@/components/RegisterForm.vue";
+import foot from "@/components/foot.vue";
+
+</script>
+
+
+<style scoped>
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+</style>
